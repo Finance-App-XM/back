4af3a8b4748767d2ab8d8957988d5c3e17824e81
Ximenes <thiagoximeneslima@gmail.com>
2023-02-22 11:46:44 -0300

    feat(Email Verification): create views to email verification, install tailwind and fix server vite config

---
+++ resources/views/emailVerification/alreadyVerified.blade.php
@@ -0,0 +1,10 @@
+@extends('app')
+@section('title', 'Email já verificado!')
+<div class="w-full flex justify-center text-center p-14 flex-col my-auto h-screen">
+    <h1 class="text-gray-700 text-3xl">
+        Seu email já está verificado!
+    </h1>
+    <p class="text-xl mt-2 text-gray-500">
+        Volte a aplicação e faça login.
+    </p>
+</div>
---
+++ resources/views/emailVerification/invalidVerification.blade.php
@@ -0,0 +1,10 @@
+@extends('app')
+@section('title', 'Invalid Link!')
+<div class="w-full flex justify-center text-center p-14 flex-col my-auto h-screen">
+    <h1 class="text-gray-700 text-3xl">
+        Link inválido!
+    </h1>
+    <p class="text-xl mt-2 text-gray-500">
+        Verifique o link ou volte ao aplicativo, faça login e peça o reenvio do email de confirmação.
+    </p>
+</div>
